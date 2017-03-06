a4924a6928facdda942844b1bac8f0a53eb9ff4b
wcgmiracle@gmail.com <wcg47230554>
2017-03-06 23:55:58 +0800

    add 1 OPP file: slots

---
+++ use_slots.py
@@ -0,0 +1,22 @@
+#!/user/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Student(object):
+    _slots_ = ('name', 'age')
+
+
+class GraduateStudent(Student):
+    pass
+
+
+s = Student()
+s.name = 'Michael'
+s.age = 15
+try:
+    s.score = 88
+except AttributeError as e:
+    print('AttributeError:', e)
+
+g = GraduateStudent()
+g.score = 99
+print(g.score)
