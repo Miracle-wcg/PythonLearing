e0629c5a0b5da2f8f6ac38989f1de1b3d75258c8
wcgmiracle@gmail.com <wcg47230554>
2017-03-13 00:18:44 +0800

    add OPP file: urllib

---
+++ use_urllib.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from urllib import request, parse
+import ssl
+
+# get:
+
+context = ssl._create_unverified_context()  # skip error SSL: CERTIFICATE_VERIFY_FAILED
+# with request.urlopen('https://api.douban.com/v2/book/2129650', context=context) as f:
+#     data = f.read()
+#     print('Status:', f.status, f.reason)
+#     for k, v in f.getheaders():
+#         print('%s: %s' % (k, v))
+#     print('Data:', data.decode('utf-8'))
+
+# advanced get:
+
+req = request.Request('http://news.163.com/17/0312/19/CFBP657F0001875O.html')
+req.add_header('User-Agent',
+               'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
+with request.urlopen(req, context=context) as f:
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', f.read().decode('utf-8'))
+
+# with proxy and proxy auth:
+
+# proxy_handler = urllib.request.ProxyHandler({'http': 'http://www.example.com:3128/'})
+# proxy_auth_handler = urllib.request.ProxyBasicAuthHandler()
+# proxy_auth_handler.add_password('realm', 'host', 'username', 'password')
+# opener = urllib.request.build_opener(proxy_handler, proxy_auth_handler)
+# with opener.open('http://www.example.com/login.html') as f:
+#     pass
